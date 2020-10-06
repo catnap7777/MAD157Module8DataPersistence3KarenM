199ee89548ffb23dd650d4645b0df174b032f17c
km <35552602+catnap7777@users.noreply.github.com>
2020-10-06 15:59:30 -0500

    initial commit

--- MAD157Module8DataPersistence3KarenM/ViewController.swift
+++ MAD157Module8DataPersistence3KarenM/ViewController.swift
@@ -7,13 +7,101 @@
 //
 
 import UIKit
+import Foundation
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var enterGuitarDescription: UITextField!
+    @IBOutlet var displayDataHere: UILabel!
+    
+    var dataManager : NSManagedObjectContext!
+    //.. array to hold the database info for loading/saving
+    var listArray = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        dataManager = appDelegate.persistentContainer.viewContext
+        displayDataHere.text?.removeAll()
+        //.. get any initial data if there is some
+        fetchData()
     }
+    
+    //.. save to db
+    @IBAction func saveRecordButton(_ sender: UIButton) {
+        
+        //.. for "Item" table in xcdatamodeld
+        let newEntity = NSEntityDescription.insertNewObject(forEntityName:"Item", into: dataManager)
+        //.. for "about" attribute/field in table "Item" in xcdatamodeld
+        newEntity.setValue(enterGuitarDescription.text!, forKey: "about")
+        
+        do{
+            //.. try to save in db
+            try self.dataManager.save()
+            //.. add new entity to array
+            listArray.append(newEntity)
+        } catch{
+            print ("Error saving data")
+        }
+        displayDataHere.text?.removeAll()
+        enterGuitarDescription.text?.removeAll()
+        //.. refetch data to redisplay
+        fetchData()
+    }
+    
+    //.. delete from db
+    @IBAction func deleteRecordButton(_ sender: UIButton) {
+        //.. set the String of what you want to delete
+        let deleteItem = enterGuitarDescription.text!
+        //.. go through entire array to search for the string you want to delete (deleteItem above)
+        for item in listArray {
+            //.. if the value for the attribute/field "about" equals deleteItem...
+            if item.value(forKey: "about") as! String == deleteItem {
+                //.. try to delete the row from what's there
+                dataManager.delete(item)
+            }
+            do {
+                //**** not sure why you're re-saving this ??? Doesn't the above do that already?
+                //.. re-save to the db
+                try self.dataManager.save()
+            } catch {
+                print ("Error deleting data")
+            }
+            displayDataHere.text?.removeAll()
+            enterGuitarDescription.text?.removeAll()
+            //.. refetch data to redisplay text field
+            fetchData()
+        }
+        
+    }
+    
+    //.. read from db
+    func fetchData() {
+        
+        //.. setup fetch from "Item" in xcdatamodeld
+        let fetchRequest : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Item")
+        do {
+            //.. try to fetch data
+            let result = try dataManager.fetch(fetchRequest)
+            //.. set the array equal to the results fetched
+            listArray = result as! [NSManagedObject]
+            
+            //.. for each item in the array, do the following..
+            for item in listArray {
+                //.. get the value for "about" (attribute/field "about" in xcdatamodeld) and set it equal to var product
+                let product = item.value(forKey: "about") as! String
+                //.. do a simple concatenation to show all products that were fetched from db
+                displayDataHere.text! += product
+
+            }
+        } catch {
+            print ("Error retrieving data")
+        }
+    }
+
+
 
 
 }
